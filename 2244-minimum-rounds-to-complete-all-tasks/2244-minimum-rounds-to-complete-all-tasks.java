40ac75a5fae816f5c7c8946a6f1105f94c7e852e
👋 Hi, I'm Shivam Choudhary <sc0626110@gmail.com>
2026-07-01 10:35:36 +0530

    Time: 94 ms (5.18%), Space: 107.4 MB (82.07%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimumRounds(int[] tasks) {
+        TreeMap<Integer,Integer> map=new TreeMap<>();
+        for(int i:tasks){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        int count=0;
+    
+        for(Map.Entry<Integer,Integer> e:map.entrySet()){
+            if(e.getValue()<=1){
+                return -1;
+            }
+            if(e.getValue()==3 || e.getValue()==2){
+                count++;
+            }
+            else if(e.getValue()>3){
+               count+=(e.getValue()+2)/3;
+            }
+        }
+        if(count==0) return -1;
+        return count;
+    }
+}
