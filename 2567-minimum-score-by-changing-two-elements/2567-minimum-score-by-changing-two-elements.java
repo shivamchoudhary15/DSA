8518bd20fe8656095998025c50af439a93ea71cd
👋 Hi, I'm Shivam Choudhary <sc0626110@gmail.com>
2026-07-01 11:09:58 +0530

    Time: 19 ms (28.12%), Space: 59.8 MB (37.5%) - LeetHub

---
+++ 2567-minimum-score-by-changing-two-elements/2567-minimum-score-by-changing-two-elements.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minimizeSum(int[] nums) {
+        int n = nums.length;
+         if(nums.length<=3) return 0;
+
+        Arrays.sort(nums);
+        return Math.min(nums[n-1]-nums[2],Math.min(nums[n-2]-nums[1],nums[n-3]-nums[0]));
+    }
+  
+}
