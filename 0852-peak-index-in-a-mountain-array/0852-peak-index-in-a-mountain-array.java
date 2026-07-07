baa56979361885f3074a184588176f96c99574dd
👋 Hi, I'm Shivam Choudhary <sc0626110@gmail.com>
2026-07-07 09:42:56 +0530

    Time: 0 ms (100%), Space: 79.8 MB (96.78%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,10 +1,16 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i]>arr[i+1]){
-                return i;
+       int l=0;
+       int r=arr.length-1;
+       while(l<r){
+        int mid=l+(r-l)/2;
+            if(arr[mid]<arr[mid+1]){
+                l=mid+1;
             }
-        }
-        return -1;
+            else if(arr[mid]>arr[mid+1]){
+                r=mid;
+            }
+       }
+       return l;
     }
 }
