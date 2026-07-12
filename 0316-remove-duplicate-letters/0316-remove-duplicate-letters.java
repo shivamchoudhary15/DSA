a080d0136f5b88d4ce791ae918d71da5cc92352e
👋 Hi, I'm Shivam Choudhary <sc0626110@gmail.com>
2026-07-12 18:41:57 +0530

    Time: 2 ms (87.35%), Space: 43.8 MB (57.84%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int l[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            l[s.charAt(i)-'a']=i;
+        }
+        boolean s1[]=new boolean[26];
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            int curr=s.charAt(i)-'a';
+            if(s1[curr]) continue;
+            while(!st.isEmpty() && st.peek()>curr && i<l[st.peek()]){
+                s1[st.pop()]=false;
+            }
+            st.push(curr);
+            s1[curr]=true;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty())
+            sb.append((char) (st.pop() +'a'));
+        return sb.reverse().toString();
+    }
+}
