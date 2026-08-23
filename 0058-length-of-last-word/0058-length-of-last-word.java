567ac119c6759dfb97e902e224115adb6e616c1e
👋 Hi, I'm Shivam Choudhary <sc0626110@gmail.com>
2026-08-23 20:16:51 +0530

    Time: 1 ms (27.72%), Space: 42.9 MB (69.43%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String a[]=s.split(" ");
+        String d=a[a.length-1];
+        return d.length();
+        
+        
+        
+        
+        
+    }
+}
