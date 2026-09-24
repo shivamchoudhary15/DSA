bb55363ea93cdc0926446356488745399dbeb2b3
👋 Hi, I'm Shivam Choudhary <sc0626110@gmail.com>
2026-09-24 09:08:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum=0;
+        for(int i:nums){
+            sum+=i;
+        }
+        int s=(target+sum)/2;
+        return knapsack(nums,s);
+    }
+    public static int knapsack(int [] nums,int target){
+        int n=nums.length;
+        int t[][]=new int[n+1][target+1];
+
+        for(int i=0;i<=n;i++){
+            t[i][0]=1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=target;j++){
+                if(nums[i-1]<=j){
+                    t[i][j]=t[i-1][j]+t[i-1][j-nums[i-1]];
+                }
+                else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return t[n][target];
+    }
+}
